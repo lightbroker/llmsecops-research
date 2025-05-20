eef8d73f7fd343c5d31afd4fffad2fba6fa76de0
Adam Wilson <lightbrok3r@gmail.com>
2025-05-20 11:21:37 -0600

    API: add POST method w/ service; test action

--- src/api/http_api.py
+++ src/api/http_api.py
@@ -1,6 +1,13 @@
+"""
+    Usage:
+        $ uvicorn src.api.http_api:app --host 0.0.0.0 --port 9999
+"""
+
 from fastapi import FastAPI
 from pathlib import Path
 from pydantic import BaseModel
+from src.llm.llm import Phi3LanguageModel
+
 
 STATIC_PATH = Path(__file__).parent.absolute() / 'static'
 
@@ -19,3 +26,10 @@ class LanguageModelResponse(BaseModel):
 @app.get('/', response_model=str)
 async def health_check():
     return 'success'
+
+
+@app.post('/api/conversations', response_model=LanguageModelResponse)
+async def get_llm_conversation_response(request: LanguageModelPrompt):
+    service = Phi3LanguageModel()
+    response = service.invoke(user_input=request.prompt)
+    return LanguageModelResponse(response=response)
