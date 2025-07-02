a25e8f9545028069ad5dd24ced2fbf775684fe4c
Adam Wilson <lightbrok3r@gmail.com>
2025-07-02 11:34:47 -0600

    updates

--- tests/static_analysis/run_static_analysis.sh
+++ tests/static_analysis/run_static_analysis.sh
@@ -1,10 +1,13 @@
+cd ../..
+pwd
+
 # get dependencies
 pip install -r ./requirements.txt
 
 # check cyclomatic complexity
-python -m mccabe --min 3 ./../src/**/*.py
+python -m mccabe --min 3 **/*.py
 
 # SAST (static application security testing)
 bandit -r ./../src
 
-mypy 
+mypy
